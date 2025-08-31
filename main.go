9836482d42877caa1d10c63e8297acfe52357b68
mamatb <amatbaeza@proton.me>
2025-08-31 20:08:20 +0200

    Learn HTTP Servers in Go 1.11

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import "net/http"
 
 func main() {
-	serverMux := http.NewServeMux()
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			w.WriteHeader(200)
+			w.Write([]byte("OK"))
+		}))
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: serverMux,
+		Handler: mux,
 	}
 	server.ListenAndServe()
 }
