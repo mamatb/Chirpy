3677dc5528d0f7ef2d1e84d93c2a2a21b4f8275c
mamatb <amatbaeza@proton.me>
2025-09-01 13:09:22 +0200

    Learn HTTP Servers in Go 2.1

--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsReset(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(200)
-			w.Write([]byte("OK"))
-		}))
+	mux, apiCfg := http.NewServeMux(), apiConfig{fileserverHits: atomic.Int32{}}
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte("OK"))
+	})
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix(
+		"/app/",
+		http.FileServer(http.Dir(".")),
+	)))
+	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte("Hits: "))
+		w.Write([]byte(strconv.Itoa((int)(apiCfg.fileserverHits.Load()))))
+	})
+	mux.Handle("/reset", apiCfg.middlewareMetricsReset(http.StripPrefix(
+		"/reset",
+		http.FileServer(http.Dir(".")),
+	)))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
