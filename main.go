587aaa779c6d03c1b146b513d71f3fa34c11af84
mamatb <amatbaeza@proton.me>
2025-09-01 16:56:28 +0200

    Learn HTTP Servers in Go 2.3

--- main.go
+++ main.go
@@ -11,23 +11,23 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
+		c.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
 }
 
-func (cfg *apiConfig) middlewareMetricsReset(next http.Handler) http.Handler {
+func (c *apiConfig) middlewareMetricsReset(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Store(0)
+		c.fileserverHits.Store(0)
 		next.ServeHTTP(w, r)
 	})
 }
 
 func main() {
 	mux, config := http.NewServeMux(), apiConfig{}
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Write([]byte("OK"))
 	})
@@ -35,12 +35,12 @@ func main() {
 		"/app/",
 		http.FileServer(http.Dir(".")),
 	)))
-	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /metrics", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.Write([]byte("Hits: "))
 		w.Write([]byte(strconv.Itoa((int)(config.fileserverHits.Load()))))
 	})
-	mux.Handle("/reset", config.middlewareMetricsReset(http.StripPrefix(
+	mux.Handle("POST /reset", config.middlewareMetricsReset(http.StripPrefix(
 		"/reset",
 		http.FileServer(http.Dir(".")),
 	)))
