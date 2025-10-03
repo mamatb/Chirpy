008c5bc178c0f3dc25ecbd5ab8e65e8d5c77f638
mamatb <amatbaeza@proton.me>
2025-10-03 21:55:37 +0200

    Learn HTTP Servers in Go 6.6

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -9,4 +15,35 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password string, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(id uuid.UUID, secret string, expiry time.Duration) (string, error) {
+	start := jwt.NumericDate{Time: time.Now()}
+	end := jwt.NumericDate{Time: start.Add(expiry)}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		Subject:   id.String(),
+		ExpiresAt: &end,
+		IssuedAt:  &start,
+	})
+	return token.SignedString([]byte(secret))
+}
+
+func ValidateJWT(tokenString string, secret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	if _, err := jwt.ParseWithClaims(
+		tokenString,
+		&claims,
+		func(token *jwt.Token) (any, error) {
+			return []byte(secret), nil
+		},
+		jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}),
+		jwt.WithIssuer("chirpy"),
+	); err != nil {
+		return uuid.Nil, err
+	} else if id, err := uuid.Parse(claims.Subject); err != nil {
+		return uuid.Nil, jwt.ErrTokenInvalidSubject
+	} else {
+		return id, nil
+	}
 }
---
+++ internal/auth/auth_test.go
@@ -0,0 +1,129 @@
+package auth
+
+import (
+	"errors"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func TestHashPassword(t *testing.T) {
+	tests := []string{
+		"123456",
+		"qwerty",
+		"secret",
+	}
+	want := regexp.MustCompile(`^\$2a\$10\$[./0-9A-Za-z]{53}$`)
+	for _, input := range tests {
+		if output, err := HashPassword(input); err != nil || !want.MatchString(output) {
+			t.Errorf(
+				"HashPassword(\"%s\") = (\"%s\", %v), want (%#q, nil)",
+				input, output, err, want,
+			)
+		}
+	}
+}
+
+func TestCheckPasswordHash(t *testing.T) {
+	testsOk, testsErr := map[string]string{}, map[string]string{}
+	tests := []string{
+		"123456",
+		"qwerty",
+		"secret",
+	}
+	for _, input := range tests {
+		output, _ := bcrypt.GenerateFromPassword([]byte(input), bcrypt.DefaultCost)
+		testsOk[input] = string(output)
+		testsErr[input] = "error"
+	}
+	for input, output := range testsOk {
+		if err := CheckPasswordHash(input, output); err != nil {
+			t.Errorf(
+				"CheckPassword(\"%s\", \"%s\") = (%v), want (nil)",
+				input, output, err,
+			)
+		}
+	}
+	for input, output := range testsErr {
+		if err := CheckPasswordHash(input, output); err == nil {
+			t.Errorf(
+				"CheckPassword(\"%s\", \"%s\") = (%v), want (error)",
+				input, output, nil,
+			)
+		}
+	}
+}
+
+func TestMakeJWT(t *testing.T) {
+	tests := []string{
+		"123456",
+		"qwerty",
+		"secret",
+	}
+	want := regexp.MustCompile(`^(eyJ[-_0-9A-Za-z]+\.){2}[-_0-9A-Za-z]+$`)
+	for _, inputSecret := range tests {
+		inputId, inputExpiry := uuid.New(), time.Second*time.Duration(60)
+		if output, err := MakeJWT(inputId, inputSecret, inputExpiry); err != nil ||
+			!want.MatchString(output) {
+			t.Errorf(
+				"MakeJWT(%s, \"%s\", %.2fs) = (\"%s\", %v), want (%#q, nil)",
+				inputId, inputSecret, inputExpiry.Seconds(), output, err, want,
+			)
+		}
+	}
+}
+
+func TestValidateJWT(t *testing.T) {
+	tests, inputToken, inputSecret := map[string]error{}, "", "secret"
+	issuer, start := "chirpy", jwt.NumericDate{Time: time.Now()}
+	end := jwt.NumericDate{Time: start.Add(time.Second * time.Duration(60))}
+	inputToken, _ = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    issuer,
+		IssuedAt:  &start,
+		ExpiresAt: &end,
+		Subject:   uuid.New().String(),
+	}).SignedString([]byte(inputSecret))
+	tests[inputToken] = nil
+	inputToken, _ = jwt.NewWithClaims(jwt.SigningMethodHS512,
+		jwt.RegisteredClaims{}).SignedString([]byte(inputSecret))
+	tests[inputToken] = jwt.ErrTokenSignatureInvalid
+	inputToken, _ = jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.RegisteredClaims{}).SignedString([]byte("error"))
+	tests[inputToken] = jwt.ErrTokenSignatureInvalid
+	inputToken, _ = jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.RegisteredClaims{}).SignedString([]byte(inputSecret))
+	tests[inputToken] = jwt.ErrTokenInvalidClaims
+	inputToken, _ = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    issuer,
+		IssuedAt:  &start,
+		ExpiresAt: &start,
+		Subject:   uuid.New().String(),
+	}).SignedString([]byte(inputSecret))
+	tests[inputToken] = jwt.ErrTokenExpired
+	inputToken, _ = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "error",
+		IssuedAt:  &start,
+		ExpiresAt: &end,
+		Subject:   uuid.New().String(),
+	}).SignedString([]byte(inputSecret))
+	tests[inputToken] = jwt.ErrTokenInvalidIssuer
+	inputToken, _ = jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    issuer,
+		IssuedAt:  &start,
+		ExpiresAt: &end,
+		Subject:   "error",
+	}).SignedString([]byte(inputSecret))
+	tests[inputToken] = jwt.ErrTokenInvalidSubject
+	for inputToken, want := range tests {
+		if output, err := ValidateJWT(inputToken, inputSecret); !errors.Is(err, want) {
+			t.Errorf(
+				"ValidateJWT(\"%s\", \"%s\") = (%s, %v), want (%s, %v)",
+				inputToken, inputSecret, output, err, output, want,
+			)
+		}
+	}
+}
